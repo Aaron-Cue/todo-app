80b363317726a6bf537f67e66ba7494517f663a0
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-01-31 23:04:00 -0300

    Refactored component to use useRef instead of useState

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -1,73 +1,51 @@
-import { useState } from "react"
+import { useRef } from "react"
 
 export default function FormModal({ handleModal, setTasks }) {
-  const [title, setTitle] = useState(null)
-  const [description, setDescription] = useState(null)
-  const [deadline, setDeadline] = useState(null)
 
-  const handleChangeTitle = (newValue) => {
-    setTitle(newValue)
-  }
+  console.log('re-render')
 
-  const handleChangeDesc = (newValue) => {
-    setDescription(newValue)
-  }
+  const titleRef = useRef()
+  const descriptionRef = useRef()
+  const deadlineRef = useRef()
 
-  const handleChangeDeadline = (newValue) => {
-    setDeadline(newValue)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault()  
 
-  const handleAddTask = () => {
-    
-    if (!title || !deadline || !description) {   
-      alert('all fields are required')
-      return
+    const title = titleRef.current.value
+    const description = descriptionRef.current.value
+    const deadline = deadlineRef.current.value
+
+    const newTask = {
+      title,
+      description,
+      deadline
     }
 
-    const newTask = { title, description, deadline }
     setTasks(prevTasks => [...prevTasks, newTask])
     
     handleModal()
-
   }
 
   return (
     <>
-      <form action="POST">
+      <form action="POST" onSubmit={(e) => handleSubmit(e)} >
         <h2>
           <i>Add new task</i>
         </h2>
         <label>
           Title
-          <input
-            required
-            type="text"
-            name="title"
-            onChange={(e) => handleChangeTitle(e.target.value)}
-          />
+          <input ref={titleRef} required type="text" name="title" />
         </label>
         <label>
           Description
-          <input
-            type="text"
-            required
-            name="description"
-            onChange={(e) => handleChangeDesc(e.target.value)}
-          />
+          <input ref={descriptionRef} type="text" required name="description" />
         </label>
         <label>
           Deadline
-          <input
-            required
-            type="date"
-            name="deadline"
-            onChange={(e) => handleChangeDeadline(e.target.value)}
-          />
+          <input ref={deadlineRef} required type="date" name="deadline" />
         </label>
         <div className="form-buttons">
-          <button type="submit" onClick={handleAddTask}>
-            Add
-          </button>
+          <button type="submit">Add</button>
           <button className="btn-cancel" onClick={handleModal}>
             Cancel
           </button>
